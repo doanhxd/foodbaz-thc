7096bc14b2f3e91dcd865bc552d5c61f666e8933
doanhxd <doanhmiu99@gmail.com>
2021-09-21 21:43:41 +0700

    Update responsive with :heart:

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    domains: ["jehangirs.restaurant", "links.papareact.com"],
+  reactStrictMode: true,
+  webpack5: false,
+  webpack: (config, { isServer }) => {
+    // Fixes packages that depend on fs/module module
+    if (!isServer) {
+      config.node = { fs: "empty", module: "empty" };
+    }
+    return config;
   },
 };
